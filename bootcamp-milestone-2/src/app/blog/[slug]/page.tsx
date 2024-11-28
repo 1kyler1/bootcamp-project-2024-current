aa541505ac4b20ba3fec0d29142e7577264444c4
Kyler <kylernord@Kylers-MBP.lan>
2024-11-27 19:52:44 -0800

    feat: finished milestone 3A, this commti contains part of my 3B, please ingnore those newer files

--- bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
@@ -1,3 +1,13 @@
+import blogPreview from "@/components/blogPreview"
+import React from "react";
+import Image from "next/image";
+import Comment from "@/components/commentView";
+import { IComment } from "@/database/blogSchema";
+
+type Props = {
+    params: { slug: string }
+}
+
 async function getBlog(slug: string) {
 	try {
 		// This fetches the blog from an api endpoint that would GET the blog
@@ -17,11 +27,34 @@ async function getBlog(slug: string) {
 		// Instead of "error: " + err, we can just do the above
 		// it is simular to formated strings in python --> f"{err}"
 	}
-}
+}  //helllo
 
-export default async function Blog({params: { slug }: Props) {
-	slug = params.slug;
-	blog = await getBlog(slug);
-	
-	return ...
-}
+export default async function Blog({ params }: Props) {
+  const { slug } = params;
+  const blog = await getBlog(slug);
+    return (
+      <div>
+        <main>
+          <main className="main">
+            <div className="title" id="blog">
+              <h1>{blog?.title}</h1>
+            </div>
+            <div className="image" id="blog">
+              <Image src={`/images/${blog?.image}`} alt="temp" height="500" width="500"/> 
+            </div>
+
+            <div className="content" id="blog">
+              <p>{blog?.content}</p>
+            </div>
+
+            <div className="comment" id="blog">
+              {blog.comments.map((comment: IComment, index: number) => (
+	                <Comment key={index} comment={comment} />
+	            ))}
+            </div>
+          </main>
+          <footer>© 2023 Kyler Nord | All Rights Reserved</footer>
+        </main>
+      </div>
+    );
+  }
