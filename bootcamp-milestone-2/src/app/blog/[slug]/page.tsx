e42696cd75968cbafcc84f08811ae566d6f309cc
Kyler <kylernord@Kylers-MBP.lan>
2024-12-19 13:56:53 -0800

    feat: here is my current code

--- bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
@@ -26,90 +26,44 @@ async function getBlog(slug: string) {
 	}
 }  
 
+
 export default async function Blog({ params: { slug } }: Props) {
-  // const { slug } = params;
-
   const blog = await getBlog(slug);
-  // if (blog?.comments) {
-  //   // Log each comment to verify they're there
-  //   blog.comments.forEach((comment: IComment) => console.log(comment));
-    return (
-      <div>
-        <main>
-          <main className="main">
-            <div className="title" id="blog">
-              <h1>{blog?.title}</h1>
-            </div>
-            <div className="image" id="blog">
-              <Image src={`/images/${blog?.image}`} alt="temp" height="500" width="500"/> 
-            </div>
 
-            <div className="content" id="blog">
-              <p>{blog?.content}</p>
-            </div>
-
-            
-            <div className="comment" id="blog">
-            {blog?.comments && blog.comments.length > 0 ? (      // check if we have blog comments before getting them
-              blog.comments.map((comment: IComment, index: number) => (
-                <Comment key={index} comment={comment} />
-              ))
-            ) : (
-              <p>Comments not found.</p>
-            )}
-            </div>
-          </main>
-          <footer>© 2023 Kylerrrrr Nord | All Rights Reserved</footer>
-        </main>
-      </div>
-    );
+  if (!blog) {
+    return <div>Failed to load blog</div>;
   }
 
+  return (
+    <div>
+      <main>
+        <h1>{blog.title}</h1>
+        <div className="image" id="blog">
+          <Image src={`/images/${blog.image}`} alt="temp" height="500" width="500"/> 
+        </div>
+        <div className="content" id="blog">
+          <p>{blog.content}</p>
+        </div>
+        <div className="comment" id="blog">
+          {blog.comments && blog.comments.length > 0 ? (
+            blog.comments.map((comment: IComment, index: number) => (
+              <Comment key={index} comment={comment} />
+            ))
+          ) : (
+            <p>Comments not found.</p>
+          )}
+        </div>
+      </main>
+      <footer>© 2023 Kylerrrrr Nord | All Rights Reserved</footer>
+    </div>
+  );
+}
 
 
 
 
 
 
-// export default async function Blog({ params }: Props) {
-//   const { slug } = params
-//   const blog = await getBlog(slug)
-
-//   if (blog?.comments) {
-//     // Log each comment to verify they're there
-//     blog.comments.forEach((comment: IComment) => console.log(comment));
-// }
-
-//   return (
-//     <div>
-//       <main>
-//         <main className="main">
-//           <div className="title" id="blog">
-//             <h1>{blog?.title}</h1>
-//           </div>
-//           <div className="image" id="blog">
-//             <Image src={`/images/${blog?.image}`} alt="temp" height="500" width="500"/> 
-//           </div>
-
-//           <div className="content" id="blog">
-//             <p>{blog?.content}</p>
-//           </div>
-
-//           <div className="comment" id="blog">
-//             {blog.comments.map((comment: IComment, index: number) => (
-//                 <Comment key={index} comment={comment} />
-//             ))}
-//           </div>
-//         </main>
-//         <footer>© 2023 Kyler Nord | All Rights Reserved</footer>
-//       </main>
-//     </div>
-//   );
-// }
-
-
-
-  
 
 
     
--- bootcamp-milestone-2/src/components/commentView.tsx
+++ bootcamp-milestone-2/src/components/commentView.tsx
@@ -15,36 +15,18 @@ function parseCommentTime(time: Date){
 }    
    
 
-// function Comment({ comment }: CommentProps) {
-//     return (
-//         <div>
-//             <h4>{comment.user}</h4>
-//             <p>{comment.comment}</p>
-//             <span>{parseCommentTime(new Date(comment.time))}</span>
-//         </div>
-//     );
-// }
-// function Comment({ comment }: CommentProps) {
-//     return (
-//         <div className={styles["comment-container"]}>
-//             <h4 className={styles["comment-header"]}>{comment.user}</h4>
-//             <p className={styles["comment-text"]}>{comment.comment}</p>
-//             <span className={styles["comment-time"]}>
-//                 {parseCommentTime(new Date(comment.time))}
-//             </span>
-//         </div>
-//     );
-// }
-
-function Comment({ comment }: { comment: IComment }) {
+function Comment({ comment }: CommentProps) {
     return (
-      <div className="comment-container">
-        <h4>{comment.user}</h4>
-        <p>{comment.comment}</p>
-        <span>{new Date(comment.time).toLocaleDateString()}</span>
-      </div>
+        <div>
+            <h4>{comment.user}</h4>
+            <p>{comment.comment}</p>
+            <span>{parseCommentTime(new Date(comment.time))}</span>
+        </div>
     );
-  }
+}
+
+
+
 
 
 export default Comment;
